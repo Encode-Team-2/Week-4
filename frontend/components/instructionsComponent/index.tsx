bea1a5e05ce2afd45754e4c357039de6750268da
demonno <demur.nodia@gmail.com>
2023-08-21 10:57:34 +0400

    Add API calls to fetch token and ballot addresses

--- frontend/components/instructionsComponent/index.tsx
+++ frontend/components/instructionsComponent/index.tsx
@@ -1,14 +1,11 @@
 import { useAccount, useNetwork } from "wagmi";
 import styles from "./instructionsComponent.module.css";
+import * as tokenJson from "../../assets/MyToken.json";
+import { useEffect, useState } from "react";
 
 export default function InstructionsComponent() {
   return (
     <div className={styles.container}>
-      <header className={styles.header_container}>
-        <div className={styles.header}>
-          <h1>My App</h1>
-        </div>
-      </header>
       <div className={styles.get_started}>
         <PageBody></PageBody>
       </div>
@@ -32,6 +29,9 @@ function WalletInfo() {
       <div>
         <p>Your account address is {address}</p>
         <p>Connected to the network {chain?.name}</p>
+
+        <TokenAddressFromApi></TokenAddressFromApi>
+        <BallotAddressFromApi></BallotAddressFromApi>
       </div>
     );
   if (isConnecting)
@@ -49,6 +49,52 @@ function WalletInfo() {
   return (
     <div>
       <p>Connect wallet to continue</p>
+    </div>
+  );
+}
+
+function TokenAddressFromApi() {
+  const [data, setData] = useState<{ address: string }>();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/token/address")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading token address from API...</p>;
+  if (!data) return <p>No token address information</p>;
+
+  return (
+    <div>
+      <p>Token address from API: {data.address}</p>
+    </div>
+  );
+}
+
+function BallotAddressFromApi() {
+  const [data, setData] = useState<{ address: string }>();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/ballot/address")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading ballot address from API...</p>;
+  if (!data) return <p>No ballot address information</p>;
+
+  return (
+    <div>
+      <p>Ballot address from API: {data.address}</p>
     </div>
   );
 }
